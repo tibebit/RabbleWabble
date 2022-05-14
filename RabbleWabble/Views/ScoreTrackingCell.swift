ad98d8109853062cdd761288c67966a411004384
tibebit <fabiotibe.bit@gmail.com>
2022-05-14 20:31:59 +0200

    feat: add configuration method for a score tracking cell

--- RabbleWabble/Views/ScoreTrackingCell.swift
+++ RabbleWabble/Views/ScoreTrackingCell.swift
@@ -10,20 +10,13 @@ import UIKit
 
 public final class ScoreTrackingCell: UITableViewCell {
     public static let identifier = String(describing: ScoreTrackingCell.self)
+
     
-    @IBOutlet weak public var correctCount: UILabel!
-    @IBOutlet weak public var incorrectCount: UILabel!
-    @IBOutlet weak public var completionDate: UILabel!
-    @IBOutlet weak public var countsContainerView: UIView!
-    
-    
-    public override func layoutSubviews() {
-        super.layoutSubviews()
-        countsContainerView.layer.cornerRadius = 10
-        countsContainerView.layer.cornerCurve = .continuous
-        countsContainerView.layer.shadowColor = UIColor.black.cgColor
-        countsContainerView.layer.shadowOffset = CGSize(width: 0, height: 10)
-        countsContainerView.layer.shadowRadius = 20.0
-        countsContainerView.layer.shadowOpacity = 0.3
+    public func configure(with score: QuestionGroup.Score) {
+        var configuration = defaultContentConfiguration()
+        configuration.image = UIImage(systemName: "checklist")
+        configuration.text = "Completed on \(score.completionDate.ddMmYy())"
+        configuration.secondaryText = "Incorrect Count: \(score.incorrectCount)\nCorrect Count: \(score.correctCount)"
+        contentConfiguration = configuration
     }
 }
